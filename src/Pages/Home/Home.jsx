5873e683a458d6ca4ec05aa6d3c02d10fac7954c
Sanket <sanketpotghan143@gmail.com>
2023-09-11 12:00:23 +0530

    installing the React Carousel to the Project and Creating a Carousel in the Home component and displaying the Carousel in the Home Component using the fetch data . Adding Cdn link of Font Awsome

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import "./Home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -11,7 +14,38 @@ const Home = () => {
             .then(data => setPopularMovies(data.results))
     }, [])
     return (
-        <div>Home</div>
+        <div>
+            <div className="poster">
+                <Carousel
+                    showThumbs={false}
+                    autoPlay={true}
+                    transitionTime={3}
+                    infiniteLoop={true}
+                    showStatus={false}
+                >
+                    {
+                        popularMovies.map(movie => (
+                            <Link style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} >
+                                <div className="posterImage">
+                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                                </div>
+                                <div className="posterImage__overlay">
+                                    <div className="posterImage__title">{movie ? movie.original_title : ""}</div>
+                                    <div className="posterImage__runtime">
+                                        {movie ? movie.release_date : ""}
+                                        <span className="posterImage__rating">
+                                            {movie ? movie.vote_average : ""}
+                                            <i className="fas fa-star" />{" "}
+                                        </span>
+                                    </div>
+                                    <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </Carousel>
+            </div>
+        </div>
     )
 }
 
